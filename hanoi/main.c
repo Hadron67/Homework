08cea93eb379ba63c64ef4e3e65e3217168fdfa7
Hydron67 <604700341@qq.com>
2015-03-18 21:29:10 +0300

    second version

--- hanoi/main.c
+++ hanoi/main.c
@@ -1,19 +1,38 @@
+// homework of c language class in uestc
+// this program is free software,you can redistrubute 
+// it and/or modify under the terms of the GNU General 
+// Public License.
 #include <stdio.h>
 #include <string.h>
-
-void hanoi(int n,int a,int b,char* output){
-	char temp[10];
+void hanoi(int n,int a,int b,long*);
+void hanoi(int n,int a,int b,long* steps){
 	int c=(a==1&&b==2||a==2&&b==1)?3:((a==1&&b==3||a==3&&b==1)?2:1);
 	if(n!=0){
-		hanoi(n-1,a,c,output);
-		sprintf(temp,"%d->%d\n\0",a,b);
-		strcat(output,temp);
-		hanoi(n-1,c,b,output);
+		hanoi(n-1,a,c,steps);
+		if(steps!=NULL) {
+			(*steps)++;
+			printf("step %ld : %d --> %d\n",*steps,a,b);
+		}
+		else{
+			printf("%d --> %d\n",a,b);
+		}
+		hanoi(n-1,c,b,steps);
 	}
 }
 int main(int agv,char* ags){
-	char output[]="\0";
-	hanoi(10,1,3,output);
-	puts(output);
-	
+	printf("========Hanoi Tower Solver========\n");
+	while(1){
+		int n;
+		long step=0;
+		printf("input the number of disks to be solved:");
+		scanf("%d",&n);
+		hanoi(n,1,3,&step);
+		printf("finished in %ld step(s).\n",step);
+		printf("would you like to calculate again?(y/n)");
+		//XXX:use scanf("%c") doesn't work,so i have to choose the following foolish way.
+		char _continue[3];
+		scanf("%s",_continue);
+		if(!strcmp(_continue,"n")) break;
+	}
+	printf("good bye.\n");
 }
